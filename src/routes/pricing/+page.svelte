dea270a9bf32443baf67617862fb2beacab14a90
0xSynesty <antoine.labonne1@gmail.com>
2023-10-19 11:41:38 +0000

    updated pricing

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -1,3 +1,41 @@
-<h1>
-    TBD
-</h1>
+<div class="m-auto leading-10 dark:text-white" style="">
+    <h1 class="text-2xl my-4">Tarification</h1>
+    <ol class="list-decimal">
+        <li>
+            <h2>Inscription Gratuite</h2>
+            <p>
+                L'inscription sur la plateforme est entièrement gratuite pour
+                les artisans et les particuliers.
+            </p>
+        </li>
+        <li>
+            <h2>Commission sur les Transactions</h2>
+            <p>
+                Les artisans paient un montant forfaitaire de <strong
+                    >75 euros par mois</strong
+                > pour accéder à la fonctionnalité de mise en relation avec les clients
+                pour les interventions physiques et 35 euros par mois pour accéder
+                aux fonctionnalités de téléconsultation. Cependant, aucun frais n'est
+                prélevé auprès des particuliers.
+            </p>
+        </li>
+        <li>
+            <h2>Paiements et Facturation</h2>
+            <p>
+            Les paiements des artisans sont effectués via une plateforme sécurisée intégrée. Les factures mensuelles sont générées automatiquement et envoyées par e-mail.
+            </p>
+        </li>
+        <li>
+            <h2>Politique d'Annulation et de Remboursement</h2>
+            <p>
+            Une politique claire d'annulation et de remboursement est mise en place pour les artisans en cas d'insatisfaction ou d'annulation de leur abonnement.
+            </p>
+        </li>
+    </ol>
+</div>
+
+<style lang="postcss">
+    h2 {
+        @apply text-xl my-4 border-b-2 border-slate-500 font-Poppins;
+    }
+</style>
